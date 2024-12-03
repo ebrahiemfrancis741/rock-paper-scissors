8ff76481d57f09d0721b299aff2d89850be9e318
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2024-12-03 17:13:32 +0200

    Add function getComputerChoice() to main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+
+  let choice = Math.ceil(Math.random() * 3); 
+  switch (choice){
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+    default:
+      return "An error has occured";
+  }
+
+}
